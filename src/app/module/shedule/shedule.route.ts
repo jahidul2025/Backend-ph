f4e2c50f59df237d5e7c04512a42083f86ccbffd
jahidul islam <jahidul20673@gmail.com>
2026-08-15 11:29:00 +0600

    feat: implement schedule management module with CRUD operations and time slot generation

---
+++ src/app/module/shedule/shedule.route.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { checkAuth } from "../../middlewere/checkAuth";
+import { Role } from "../../../generated/client/enums";
+import { validateRequest } from "../../middlewere/validateRequest";
+import { ScheduleController } from "./shedule.controller";
+import { ScheduleValidation } from "./shedule.validation";
+
+const router = Router();
+
+router.post('/', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), validateRequest(ScheduleValidation.createScheduleZodSchema), ScheduleController.createSchedule);
+router.get('/', checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.DOCTOR), ScheduleController.getAllSchedules);
+router.get('/:id', checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.DOCTOR), ScheduleController.getScheduleById);
+router.patch('/:id', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), validateRequest(ScheduleValidation.updateScheduleZodSchema), ScheduleController.updateSchedule);
+router.delete('/:id', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), ScheduleController.deleteSchedule);
+
+export const scheduleRoutes = router;
